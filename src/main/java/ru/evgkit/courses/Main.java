cb4e5a47c1c6e2f9f318ad540588fba7477a999e
evgkit <kitaev.ea@gmail.com>
2017-07-08 20:56:34 +0300

    First commit

---
+++ src/main/java/ru/evgkit/courses/Main.java
@@ -0,0 +1,27 @@
+package ru.evgkit.courses;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
