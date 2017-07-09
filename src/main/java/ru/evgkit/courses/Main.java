804a9484372ac6ec0ac9b779b4c80a3d80c4a4c4
evgkit <kitaev.ea@gmail.com>
2017-07-09 11:33:38 +0300

    Another comprehensive commit

--- src/main/java/ru/evgkit/courses/Main.java
+++ src/main/java/ru/evgkit/courses/Main.java
@@ -1,5 +1,8 @@
 package ru.evgkit.courses;
 
+import ru.evgkit.courses.model.CourseIdea;
+import ru.evgkit.courses.model.CourseIdeaDAO;
+import ru.evgkit.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -10,6 +13,10 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
@@ -17,11 +24,25 @@ public class Main {
         }, new HandlebarsTemplateEngine());
 
         post("/sign-in", (req, res) -> {
-            Map<String, String> model = new HashMap<>();
-            String username = req.queryParams("username");
-            res.cookie("username", username);
-            model.put("username", username);
-            return new ModelAndView(model, "index.hbs");
+            res.cookie("username", req.queryParams("username"));
+            res.redirect("/");
+            return null;
+        });
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+
+            return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            dao.add(new CourseIdea(
+                    req.queryParams("title"),
+                    req.cookie("username"))
+            );
+            res.redirect("/ideas");
+            return null;
+        });
     }
 }
