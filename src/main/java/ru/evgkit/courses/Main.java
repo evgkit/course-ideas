1eb9fbf3f66989a9aa3845989483f906e0a59fa3
evgkit <kitaev.ea@gmail.com>
2017-07-09 14:27:39 +0300

    Another comprehensive commit

--- src/main/java/ru/evgkit/courses/Main.java
+++ src/main/java/ru/evgkit/courses/Main.java
@@ -17,31 +17,44 @@ public class Main {
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
-        get("/", (req, res) -> {
+        before((request, response) -> {
+            String username = request.cookie("username");
+            if (null != username) {
+                request.attribute("username", username);
+            }
+        });
+
+        get("/", (request, response) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", request.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/sign-in", (req, res) -> {
-            res.cookie("username", req.queryParams("username"));
-            res.redirect("/");
+        post("/sign-in", (request, response) -> {
+            response.cookie("username", request.queryParams("username"));
+            response.redirect("/");
             return null;
         });
 
-        get("/ideas", (req, res) -> {
+        before("/ideas", (request, response) -> {
+            if (null == request.attribute("username")) {
+                response.redirect("/");
+            }
+        });
+
+        get("/ideas", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("ideas", dao.findAll());
 
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/ideas", (req, res) -> {
+        post("/ideas", (request, response) -> {
             dao.add(new CourseIdea(
-                    req.queryParams("title"),
-                    req.cookie("username"))
+                    request.queryParams("title"),
+                    request.attribute("username"))
             );
-            res.redirect("/ideas");
+            response.redirect("/ideas");
             return null;
         });
     }
